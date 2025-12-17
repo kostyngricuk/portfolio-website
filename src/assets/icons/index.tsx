1f223566d1eaac9aeda02de9a456acd72f599bb7
Kanstantsin Hrytsuk <kanstantsin.hrytsuk@instinctools.com>
2025-12-17 12:00:14 +0300

    feat(icons): update GitHub icon and add LinkedIn, Telegram icons

--- src/assets/icons/index.tsx
+++ src/assets/icons/index.tsx
@@ -1,4 +1,5 @@
 export { default as SunIcon } from './sun.svg';
 export { default as MoonIcon } from './moon.svg'
 export { default as GitHubIcon } from './github.svg'
-export { default as GitHubWhiteIcon } from './github-white.svg'
+export { default as TelegramIcon } from './telegram.svg'
+export { default as LinkedInIcon } from './linkedin.svg';
